ab4f1389221484b503d37a28be711444fd769330
Aman <Palaman2626@gmail.com>
2023-07-02 20:40:19 +0530

    uploading Portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-
+import { Text } from '@react-three/drei'
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
+    <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
+      <color attach="background" args={["#dfd7d7"]} />
+      
+      <Text
+        font="fonts/Poppins-Black.ttf"
+        fontSize={0.3}
+        position={[0, 2.4, 0.13]}
+        anchorX={"center"}
+      >
+        Double Click on the Cards to open the world
+        <meshBasicMaterial color={'#444244'} toneMapped={false} />
+      </Text>
       <Experience />
     </Canvas>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,148 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  Environment,
+  MeshPortalMaterial,
+  RoundedBox,
+  useTexture,
+  Text,
+  CameraControls,
+  useCursor,
+} from "@react-three/drei";
+import * as THREE from "three";
+import { Fish } from "./Fish";
+import { Cactoro } from "./Cactoro";
+import { Dragon_Evolved } from "./Dragon_Evolved";
+import { useState } from "react";
+import { useRef, useEffect } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { easing } from "maath";
 
 export const Experience = () => {
+  const scene = useThree((state) => state.scene);
+  const [active, setActive] = useState(null);
+  const [hovered, setHovered] = useState(null);
+  useCursor(hovered);
+  const controlsRef = useRef();
+
+  useEffect(() => {
+    if (active) {
+      const targetPosition = new THREE.Vector3();
+      scene.getObjectByName(active).getWorldPosition(targetPosition);
+      controlsRef.current.setLookAt(
+        0,
+        0,
+        5,
+        targetPosition.x,
+        targetPosition.y,
+        targetPosition.z,
+        true
+      );
+    } else {
+      controlsRef.current.setLookAt(0, 0, 10, 0, 0, 5, true);
+    }
+  }, [active]);
+
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      <Environment preset="sunset" />
+      <ambientLight intensity={0.5} />
+      <CameraControls
+        ref={controlsRef}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 6}
+      />
+
+      <MonsterStage
+        texture={"textures/anime_art_style_beach.jpg"}
+        color={"#3d5a80"}
+        name="Fish"
+        active={active}
+        setActive={setActive}
+        hovered={hovered}
+        setHovered={setHovered}
+      >
+        <Fish scale={0.6} position-y={-1} hovered={ hovered === 'Fish'}/>
+      </MonsterStage>
+
+      <MonsterStage
+        texture={"textures/anime_art_style_mountains.jpg"}
+        position-x={-2.5}
+        rotation-y={Math.PI / 8}
+        color={"#739d3c"}
+        name="Cactoro"
+        active={active}
+        setActive={setActive}
+        hovered={hovered}
+        setHovered={setHovered}
+      >
+        <Cactoro scale={0.45} position-y={-1} hovered={hovered === 'Cactoro'}/>
+      </MonsterStage>
+
+      <MonsterStage
+        texture={"textures/anime_art_style_lava_fire.jpg"}
+        position-x={2.5}
+        rotation-y={-Math.PI / 8}
+        color={"#df8d52"}
+        name="Dragon"
+        active={active}
+        setActive={setActive}
+        hovered={hovered}
+        setHovered={setHovered}
+      >
+        <Dragon_Evolved scale={0.5} position-y={-1} hovered={hovered === 'Dragon'} />
+      </MonsterStage>
     </>
+  );
+};
+
+const MonsterStage = ({
+  children,
+  texture,
+  name,
+  color,
+  active,
+  setActive,
+  hovered,
+  setHovered,
+  ...props
+}) => {
+  const map = useTexture(texture);
+  const portalMaterial = useRef();
+  useFrame((_state, delta) => {
+    const worldOpen = active === name;
+    easing.damp(portalMaterial.current, "blend", worldOpen ? 1 : 0, 0.2, delta);
+  });
+
+  return (
+    <group {...props}>
+      {/* To write the name of the card on it */}
+      <Text
+        font="fonts/Caprasimo-Regular.ttf"
+        fontSize={0.3}
+        position={[0, -1.4, 0.13]}
+        anchorY={"bottom"}
+      >
+        {name}
+        <meshBasicMaterial color={color} toneMapped={false} />
+      </Text>
+
+      {/* The actual portal box */}
+      <RoundedBox
+        name={name}
+        args={[2, 3, 0.2]}
+        onDoubleClick={() => setActive(active === name ? null : name)}
+        onPointerEnter={()=>setHovered(name)}
+        onPointerLeave={()=>setHovered(null)}
+      >
+        <MeshPortalMaterial ref={portalMaterial} side={THREE.DoubleSide}>
+          <ambientLight intensity={0.5} />
+          <Environment preset="sunset" />
+          {children}
+          <mesh>
+            <sphereGeometry args={[5, 32, 32]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
+    </group>
   );
 };
