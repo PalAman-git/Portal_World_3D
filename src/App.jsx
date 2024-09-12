cd99cbea9f547464ade7f6b77f7a1b6d6e702f70
Aman <Palaman2626@gmail.com>
2024-09-13 00:43:00 +0530

    background to black

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import { Text } from '@react-three/drei'
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 0, 10], fov: 30 }}>
-      <color attach="background" args={["#dfd7d7"]} />
+      <color attach="background" args={["#181717"]} />
       
       <Text
         font="fonts/Poppins-Black.ttf"
         fontSize={0.3}
         position={[0, 2.4, 0.13]}
         anchorX={"center"}
+        color={"#e1dae1"}
       >
-        Double Click on the Cards to open the world
+        Double Click on the Cards   &   drag to see the magic
         <meshBasicMaterial color={'#444244'} toneMapped={false} />
       </Text>
       <Experience />
